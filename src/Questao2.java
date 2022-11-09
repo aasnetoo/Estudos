8d5642eedbe76c42ba0ea8064f03e28d0dd56593
aasnetoo <115680563+aasnetoo@users.noreply.github.com>
2022-11-08 21:18:38 -0300

    Alteração da constante.

--- src/Questao2.java
+++ src/Questao2.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Questao2 {
 
+    private static final double CONSTANTE = 0.23;
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int idade = scan.nextInt();
         scan.close();
-        final double CONSTANTE = 0.23;
         double soma = (double) idade +CONSTANTE;
         System.out.println("O número da sorte é: "+soma);
 
