eb9a57b4a0c32883f4a755ae492a5f905e919bb1
aasnetoo <115680563+aasnetoo@users.noreply.github.com>
2022-11-09 10:25:44 -0300

    Nome do método.

--- src/GastoTrimestre.java
+++ src/GastoTrimestre.java
@@ -3,13 +3,13 @@ public class GastoTrimestre {
     private static final int FEVEREIRO = 23000;
     private static final int MARCO = 17000;
 
-    private static int SomaTrimestral(){
+    private static int somaTrimestral(){
         return (JANEIRO+FEVEREIRO+MARCO);
     }
 
     public static void main(String[] args) {
 
-        System.out.println("A empresa XPTO S.A teve um gasto total  no primeiro trimestre de: R$"+SomaTrimestral()+".");
+        System.out.println("A empresa XPTO S.A teve um gasto total  no primeiro trimestre de: R$"+somaTrimestral()+".");
 
     }
 }
