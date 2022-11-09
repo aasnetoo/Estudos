34912bd2f4e94a29b6414d4c3c6a48d0ff10c53f
aasnetoo <115680563+aasnetoo@users.noreply.github.com>
2022-11-09 16:16:40 -0300

    Const

--- src/Questao2.java
+++ src/Questao2.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Questao2 {
 
-    private static final double CONSTANTE = 0.23;
+    private static final double CONSTANTE = 0.023;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int idade = scan.nextInt();
         scan.close();
-        double soma = (double) idade +CONSTANTE;
+        double soma = (double) idade + CONSTANTE;
         System.out.println("O número da sorte é: "+soma);
 
     }
